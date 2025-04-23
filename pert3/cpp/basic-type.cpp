2cc91eea8d09d250e58c227fca19b1a94d124e4d
shinta <deshintapracilia@gmail.com>
2025-04-23 19:17:13 +0700

    catatan pertemuan 3

---
+++ pert3/cpp/basic-type.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int num = 10;
+
+    float b = 3.14;
+
+    double c = 9.00;
+
+    char d = 'A';
+
+    bool e = true;
+
+    string g = "hello word";
+
+    cout<< num << endl;
+    cout<< b << endl;
+    cout<< c << endl;
+    cout<< d << endl;
+    cout<< e << endl;
+    cout<< g << endl;
+}
